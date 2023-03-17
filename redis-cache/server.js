4027d2e2bf74b1d26ca530f0ca983a9dc9bec373
gustavobetete <gustavobetete@hotmail.com>
2023-03-17 09:48:38 -0300

    feat: metodo de atualizar o cache sem ter que excluir o cache antigo

--- redis-cache/server.js
+++ redis-cache/server.js
@@ -5,7 +5,7 @@ import { createClient} from "redis";
 const client = createClient()
 
 const getAllPducts = async() => {
-    const time = Math.random() * 5000
+    const time = Math.random() * 10000
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve(['Produto 1', 'Produto 2'])
@@ -19,7 +19,22 @@ app.get('/saved', async(req, res) => { // exemplo deletando o cache assim que fo
 })
 
 app.get('/', async (req, res) => {
-    const productsFromCache = await client.get('getAllPducts')
+    const productsFromCache = await client.get("getAllPducts")
+    const isProductsFromCacheStale = !(await client.get("getAllProducts:validation"))
+    if(isProductsFromCacheStale){ // em ambiente de produção precisa por em uma mensageria para ele fazer isso por vc
+        const isRefetching = !!(await client.get("getAllPducts:is-refetching"))
+        console.log({ isRefetching })
+        if(!isRefetching){
+            await client.set("getAllPducts:is-refetching", "true", { EX: 20 })
+            setTimeout(async() => {
+                console.log('Cache is stale - refetching...')
+                const products = await getAllPducts()
+                await client.set("getAllPducts", JSON.stringify(products));
+                await client.set("getAllProducts:validation", "true", { EX: 5 })
+                await client.del("getAllPducts:is-refetching");
+            }, 0)
+        }
+    }
     if(productsFromCache){
         return res.send(JSON.parse(productsFromCache));
     }
